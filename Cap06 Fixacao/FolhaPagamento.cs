d58124cb1f4cbef985a7887cd5a37528f3574f9b
Willyan Santos <willyan41@hotmail.com>
2020-04-01 17:46:19 -0300

    Adicionar arquivos de projeto.

---
+++ Cap06 Fixacao/FolhaPagamento.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Cap06_Fixacao
+{
+    class FolhaPagamento
+    {
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+
+        public FolhaPagamento(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+
+        }
+
+        public void AumentarSalario(int id, double aumento)
+        {
+
+            Salario = (Salario * aumento / 100.0) + Salario;
+        }
+
+
+        public override string ToString()
+        {
+            return Id + ", " + Nome + ", R$ " + Salario.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+
+    }
+}
---
+++ Cap06 Fixacao/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Cap06_Fixacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<FolhaPagamento> N = new List<FolhaPagamento>();
+
+
+            Console.Write("Digite a quantidade de colaboradores:");
+            int Colaborador = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < Colaborador; i++)
+            {
+                Console.Write("Informe a id do colaborador: ");
+                int Matricula = int.Parse(Console.ReadLine());
+                Console.Write("Informe o nome do colaborador: ");
+                string Nome = Console.ReadLine();
+                Console.Write("Informe o Salario do colaborador: ");
+                double Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.WriteLine();
+                N.Add(new FolhaPagamento(Matricula, Nome, Salario));
+
+
+            }
+
+            Console.Write("Informe a Id do colaborador: ");
+            int Matricula1 = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            FolhaPagamento emp1 = N.Find(x => x.Id == Matricula1);
+
+            if (emp1 != null)
+            {
+
+                Console.Write("Informe o percentual que deseja aumentar: ");
+                double Porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                emp1.AumentarSalario(Matricula1, Porcentagem);
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Id não existente.");
+                Console.WriteLine();
+            }
+            foreach (var item in N)
+            {
+                Console.WriteLine(item);
+
+            }
+
+
+        }
+    }
+}
